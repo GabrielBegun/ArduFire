b18ef75378f12be9f070d9215a0390bbf2771e7c
Gabriel Begun <begunbegun@gmail.com>
2014-02-14 18:55:23 -0500

    First update

---
+++ UserVariables.h
@@ -0,0 +1,12 @@
+/// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+// user defined variables
+
+// example variables used in Wii camera testing - replace with your own
+// variables
+#ifdef USERHOOK_VARIABLES
+
+
+#endif  // USERHOOK_VARIABLES
+
+
